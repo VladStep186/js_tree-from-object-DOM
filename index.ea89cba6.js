0e37047b3dd016b2e2556d4345e869816120e3ed
Vladyslav Stepanenko <vlad.step.186@gmail.com>
2025-07-08 14:08:38 +0300

    make build

---
+++ index.ea89cba6.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var l=document.createElement("li");l.textContent=a,"object"==typeof t[a]&&null!==t[a]&&Object.keys(t[a]).length>0&&e(l,t[a]),r.append(l)}n.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.ea89cba6.js.map
